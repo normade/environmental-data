2c07fcea00fa6dfbc43ffbf32a4f5f6ad3249aec
Norma Driske <norma@fastmail.com>
2018-08-13 16:55:24 +0200

    Added tempstation call

--- boot.py
+++ boot.py
@@ -1,9 +1,14 @@
 """This file is executed on every boot (including wake-boot from deepsleep)."""
 # import esp
 # esp.osdebug(None)
+from utime import sleep
 from wifi import wifi_stat
 import gc
+import tempstation
 # import webrepl\n
 # webrepl.start()\n
 gc.collect()
 wifi_stat.connect()
+while not wifi_stat.station.isconnected():
+    sleep(1)
+tempstation.main()
